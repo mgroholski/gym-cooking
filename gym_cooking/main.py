529b4cada8392190bac4c7b60c6aaa7471875b87
Matthew Groholski <mgrohols@asu.edu>
2026-01-21 13:10:49 -0800

    Added conda and made updates to versions for Apple Silicon.

--- gym_cooking/main.py
+++ gym_cooking/main.py
@@ -1,18 +1,17 @@
 # from environment import OvercookedEnvironment
 # from gym_cooking.envs import OvercookedEnvironment
-from recipe_planner.recipe import *
-from utils.world import World
-from utils.agent import RealAgent, SimAgent, COLORS
-from utils.core import *
-from misc.game.gameplay import GamePlay
-from misc.metrics.metrics_bag import Bag
-
-import numpy as np
-import random
 import argparse
+import random
 from collections import namedtuple
 
 import gym
+import numpy as np
+from misc.game.gameplay import GamePlay
+from misc.metrics.metrics_bag import Bag
+from recipe_planner.recipe import *
+from utils.agent import COLORS, RealAgent, SimAgent
+from utils.core import *
+from utils.world import World
 
 
 def parse_arguments():
@@ -21,31 +20,91 @@ def parse_arguments():
     # Environment
     parser.add_argument("--level", type=str, required=True)
     parser.add_argument("--num-agents", type=int, required=True)
-    parser.add_argument("--max-num-timesteps", type=int, default=100, help="Max number of timesteps to run")
-    parser.add_argument("--max-num-subtasks", type=int, default=14, help="Max number of subtasks for recipe")
+    parser.add_argument(
+        "--max-num-timesteps",
+        type=int,
+        default=100,
+        help="Max number of timesteps to run",
+    )
+    parser.add_argument(
+        "--max-num-subtasks",
+        type=int,
+        default=14,
+        help="Max number of subtasks for recipe",
+    )
     parser.add_argument("--seed", type=int, default=1, help="Fix pseudorandom seed")
-    parser.add_argument("--with-image-obs", action="store_true", default=False, help="Return observations as images (instead of objects)")
+    parser.add_argument(
+        "--with-image-obs",
+        action="store_true",
+        default=False,
+        help="Return observations as images (instead of objects)",
+    )
 
     # Delegation Planner
-    parser.add_argument("--beta", type=float, default=1.3, help="Beta for softmax in Bayesian delegation updates")
+    parser.add_argument(
+        "--beta",
+        type=float,
+        default=1.3,
+        help="Beta for softmax in Bayesian delegation updates",
+    )
 
     # Navigation Planner
     parser.add_argument("--alpha", type=float, default=0.01, help="Alpha for BRTDP")
     parser.add_argument("--tau", type=int, default=2, help="Normalize v diff")
-    parser.add_argument("--cap", type=int, default=75, help="Max number of steps in each main loop of BRTDP")
-    parser.add_argument("--main-cap", type=int, default=100, help="Max number of main loops in each run of BRTDP")
+    parser.add_argument(
+        "--cap",
+        type=int,
+        default=75,
+        help="Max number of steps in each main loop of BRTDP",
+    )
+    parser.add_argument(
+        "--main-cap",
+        type=int,
+        default=100,
+        help="Max number of main loops in each run of BRTDP",
+    )
 
     # Visualizations
-    parser.add_argument("--play", action="store_true", default=False, help="Play interactive game with keys")
-    parser.add_argument("--record", action="store_true", default=False, help="Save observation at each time step as an image in misc/game/record")
+    parser.add_argument(
+        "--play",
+        action="store_true",
+        default=False,
+        help="Play interactive game with keys",
+    )
+    parser.add_argument(
+        "--record",
+        action="store_true",
+        default=True,
+        help="Save observation at each time step as an image in misc/game/record",
+    )
 
     # Models
     # Valid options: `bd` = Bayes Delegation; `up` = Uniform Priors
     # `dc` = Divide & Conquer; `fb` = Fixed Beliefs; `greedy` = Greedy
-    parser.add_argument("--model1", type=str, default=None, help="Model type for agent 1 (bd, up, dc, fb, or greedy)")
-    parser.add_argument("--model2", type=str, default=None, help="Model type for agent 2 (bd, up, dc, fb, or greedy)")
-    parser.add_argument("--model3", type=str, default=None, help="Model type for agent 3 (bd, up, dc, fb, or greedy)")
-    parser.add_argument("--model4", type=str, default=None, help="Model type for agent 4 (bd, up, dc, fb, or greedy)")
+    parser.add_argument(
+        "--model1",
+        type=str,
+        default=None,
+        help="Model type for agent 1 (bd, up, dc, fb, or greedy)",
+    )
+    parser.add_argument(
+        "--model2",
+        type=str,
+        default=None,
+        help="Model type for agent 2 (bd, up, dc, fb, or greedy)",
+    )
+    parser.add_argument(
+        "--model3",
+        type=str,
+        default=None,
+        help="Model type for agent 3 (bd, up, dc, fb, or greedy)",
+    )
+    parser.add_argument(
+        "--model4",
+        type=str,
+        default=None,
+        help="Model type for agent 4 (bd, up, dc, fb, or greedy)",
+    )
 
     return parser.parse_args()
 
@@ -54,15 +113,16 @@ def fix_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
 
+
 def initialize_agents(arglist):
     real_agents = []
 
-    with open('utils/levels/{}.txt'.format(arglist.level), 'r') as f:
+    with open("utils/levels/{}.txt".format(arglist.level), "r") as f:
         phase = 1
         recipes = []
         for line in f:
-            line = line.strip('\n')
-            if line == '':
+            line = line.strip("\n")
+            if line == "":
                 phase += 1
 
             # phase 2: read in recipe list
@@ -72,15 +132,17 @@ def initialize_agents(arglist):
             # phase 3: read in agent locations (up to num_agents)
             elif phase == 3:
                 if len(real_agents) < arglist.num_agents:
-                    loc = line.split(' ')
+                    loc = line.split(" ")
                     real_agent = RealAgent(
-                            arglist=arglist,
-                            name='agent-'+str(len(real_agents)+1),
-                            id_color=COLORS[len(real_agents)],
-                            recipes=recipes)
+                        arglist=arglist,
+                        name="agent-" + str(len(real_agents) + 1),
+                        id_color=COLORS[len(real_agents)],
+                        recipes=recipes,
+                    )
                     real_agents.append(real_agent)
 
     return real_agents
+
 
 def main_loop(arglist):
     """The main loop for running experiments."""
@@ -108,15 +170,16 @@ def main_loop(arglist):
             agent.refresh_subtasks(world=env.world)
 
         # Saving info
-        bag.add_status(cur_time=info['t'], real_agents=real_agents)
-
+        bag.add_status(cur_time=info["t"], real_agents=real_agents)
 
     # Saving final information before saving pkl file
     bag.set_collisions(collisions=env.collisions)
-    bag.set_termination(termination_info=env.termination_info,
-            successful=env.successful)
+    bag.set_termination(
+        termination_info=env.termination_info, successful=env.successful
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     arglist = parse_arguments()
     if arglist.play:
         env = gym.envs.make("gym_cooking:overcookedEnv-v0", arglist=arglist)
@@ -125,9 +188,9 @@ if __name__ == '__main__':
         game.on_execute()
     else:
         model_types = [arglist.model1, arglist.model2, arglist.model3, arglist.model4]
-        assert len(list(filter(lambda x: x is not None,
-            model_types))) == arglist.num_agents, "num_agents should match the number of models specified"
+        assert (
+            len(list(filter(lambda x: x is not None, model_types)))
+            == arglist.num_agents
+        ), "num_agents should match the number of models specified"
         fix_seed(seed=arglist.seed)
         main_loop(arglist=arglist)
-
-
