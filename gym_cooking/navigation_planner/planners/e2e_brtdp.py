19eef7108759272ba5e483d7fc04b91a271ceada
Matthew Groholski <mgrohols@asu.edu>
2026-04-11 18:07:58 -0700

    Updated comm cost.

--- gym_cooking/navigation_planner/planners/e2e_brtdp.py
+++ gym_cooking/navigation_planner/planners/e2e_brtdp.py
@@ -82,7 +82,7 @@ class E2E_BRTDP:
         # Setting up costs for value function.
         self.time_cost = 1.0
         self.action_cost = 0.1
-        self.comm_cost = 0.099
+        self.comm_cost = 0.08
 
     def __copy__(self):
         copy_ = E2E_BRTDP(
